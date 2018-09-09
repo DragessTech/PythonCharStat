00873c65b877143124d0f97b5af7bc1f011a106d
DragessTech <39474275+DragessTech@users.noreply.github.com>
2018-09-09 13:29:14 -0700

    Add files via upload
    Python File Creation

---
+++ app.py
@@ -0,0 +1,14 @@
+print("Hello, and welcome to Character Stat Creator!")
+name = input("Please enter your name: ")
+age = int(input("Please enter your age"))
+sex = input("Are you a boy, or a girl")
+siblings = int(input("How many siblings do you have?"))
+work = int(input("How many days a week do you work?"))
+school = int(input("How many years did you spend in school"))
+self = int(input("In a scale of 1 - 10, how would you rate yourself?"))
+race = 0
+
+class years(
+    if age<10:
+        return("This worked")
+    )
