aa80bcfaa9066d00f7b79b0c3adfa02fb69436cc
root <root@localhost.localdomain>
2018-09-30 15:27:36 -0700

    Rearranged code

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
-print("Hello, and welcome to Character Stat Creator!")
-name = input("Please enter your name: ")
-age = int(input("Please enter your age: "))
-sex = input("Are you a boy, or a girl: ")
-siblings = int(input("How many siblings do you have? "))
-work = int(input("How many days a week do you work? "))
-school = int(input("How many years did you spend in school? "))
-self = int(input("In a scale of 1 - 10, how would you rate yourself? "))
+#Initializing stats
+strength = 0
+constitution = 0
+wisdom = 0
+intelligence = 0
+dexterity = 0
+charisma = 0
 race = 0
 
-def years(age):
-    if age < 10:
-        print("Hello")
+#Create function to show Stats when called
+def stats():
+    print("Name = " + name)
+    print("Age = " + age)
+    print("Race = " + Race)
+    print("Class = " + Class)
+    print("STR = " + strength)
+    print("DEX = " + dexterity)
+    print("CON = " + constitution)
+    print("INT = " + intelligence)
+    print("WIS = " + wisdom)
+    print("CHR = " + charisma)
 
-    elif age > 10:
-        print("Good bye")
-years()
+#taking input from user
+print("Hello, and welcome to Character Stat Creator!")
+name = raw_input("Please enter your name: ")
+age = input("Please enter your age: ")
+sex = raw_input("Are you a boy, or a girl: ")
+siblings = input("How many siblings do you have? ")
+work = input("How many days a week do you work? ")
+school = input("How many years did you spend in school? ")
+self = input("In a scale of 1 - 10, how would you rate yourself? ")
+Class = "Warrior"
+Race = "Human"
+
+
+
+#Runs stats function to show stats
+stats()
